446cdf338bc4a6cf577a1dfef2aecc86f814c7b3
Oleksandr Churychev <churychev.oleksandr@gmail.com>
2020-05-20 11:56:40 +0200

    Added pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <router-view />
+  <div id="app">
+    <component :is="layout">
+      <router-view />
+    </component>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import AuthLayout from "@/layouts/AuthLayout";
+import MainLayout from "@/layouts/MianLayout";
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || "auth") + "-layout";
+    }
+  },
+  components: {
+    AuthLayout,
+    MainLayout
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,10 +9,60 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: { layout: "auth" },
+    component: () => import("../views/Login")
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "auth" },
+    component: () => import("../views/Register")
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories")
+  },
+  {
+    path: "/details",
+    name: "details",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecord")
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History")
+  },
+  {
+    path: "/planing",
+    name: "planing",
+    meta: { layout: "main" },
+    component: () => import("../views/Planing")
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile")
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record")
   }
 ];
 
 const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes
 });
 
